23ab32841b9f6acf58f05790367ea2e99ee0ae6a
theartistboe <ieliea3508@gmail.com>
2025-05-25 19:57:02 -0700

    added dictionary js and css component with search function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        Hello
+        Boe's Dictionary App
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Isaboe Hollis</small>
+      </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
